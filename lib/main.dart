801dbd44ae1bea9df6634c4f7d6b7cbd840e4fe7
raneem <raneemhossam66@gmail.com>
2020-08-14 15:20:32 +0300

    Open new page when bookmark clicked feature added

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,12 @@ class _HomePageState extends State<HomePage> {
           child: Card(
               child: InkWell(
                 onTap: () {
-                  _launchURL(bookmarkList[index].link);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(bookmark: bookmarkList[index]),
+                    ),
+                  );
                 },
                 child: IntrinsicHeight(
                   child: Row(
@@ -92,11 +97,37 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  _launchURL(String url) async {
+  /*_launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw 'Could not launch $url';
     }
+  }*/
+}
+
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo.
+  final Bookmark bookmark;
+
+  // In the constructor, require a Todo.
+  DetailScreen({Key key, @required this.bookmark}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(bookmark.title, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+            SizedBox(height: 20,),
+            Text(bookmark.description, style: TextStyle(fontSize: 20),),
+          ],
+        ),
+      ),
+    );
   }
 }
