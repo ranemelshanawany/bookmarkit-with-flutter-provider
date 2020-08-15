e5f4353f5321f84eb761fd3225f0db1b357ea56e
raneem <raneemhossam66@gmail.com>
2020-08-15 05:06:30 +0300

    Using class to make app work

--- lib/bookmark_class.dart
+++ lib/bookmark_class.dart
@@ -2,8 +2,20 @@ class Bookmark
 {
   String title;
   String description;
-  String imageName;
-  String link;
+  String url;
+  String urlToImage;
+  String content;
 
-  Bookmark(this.title, this.description, this.imageName, this.link);
+  Bookmark(this.title, this.description, this.url, this.urlToImage, this.content);
+
+  Bookmark.fromJson(Map<String, dynamic> json)
+  {
+
+    title= json['title'];
+    description= json['description'];
+    url= json['url'];
+    urlToImage= json['urlToImage'];
+    content= json['content'];
+
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'bookmark_class.dart';
-import 'data.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -26,32 +26,73 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Bookmark> bookmarkList = new List(MyData.count);
+  final String url =
+      'http://newsapi.org/v2/everything?q=apple&from=2020-08-13&to=2020-08-13&sortBy=popularity&apiKey=eb9535af01634676a4492abdb5b6c3b4';
+  Future<List> bookmarkList;
+
+  /*List<Bookmark> parseBookmarks(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+    return parsed.map<Bookmark>((json) => Bookmark.fromJson(json)).toList();
+  }*/
+  //List data;
+  //Bookmark book;
+
+  List<Bookmark> _bookmarks = List<Bookmark>();
+
+  Future<List<Bookmark>> getBookmark() async {
+    var response = await http.get(url);
+    var bookmarks = List<Bookmark>();
+
+    if (response.statusCode == 200) {
+      /*setState(() {
+        var convertDataToJson = jsonDecode(response.body);
+        bookmarkList = convertDataToJson["articles"];
+      });*/
+      //return parseBookmarks(response.body);
+      var bookmarksJson = json.decode(response.body)['articles'];
+      for (var bookmarkJson in bookmarksJson) {
+        bookmarks.add(Bookmark.fromJson(bookmarkJson));
+      }
+    }
+    return bookmarks;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getBookmark().then((value) {
+      setState(() {
+        _bookmarks.addAll(value);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    makeBookmarkList();
+    //makeBookmarkList();
 
     return ListView.builder(
-      itemCount: bookmarkList.length,
+      itemCount: _bookmarks == null ? 0 : _bookmarks.length,
+      //future: getBookmark(),
       itemBuilder: (context, index) {
         return Container(
           height: 200,
           child: Card(
               child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailScreen(bookmark: bookmarkList[index]),
-                    ),
-                  );
-                },
-                child: IntrinsicHeight(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      DetailScreen(bookmark: _bookmarks[index]),
+                ),
+              );
+            },
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
                   Expanded(
                     child: Container(
                       padding: EdgeInsets.all(12),
@@ -60,50 +101,38 @@ class _HomePageState extends State<HomePage> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           Text(
-                            bookmarkList[index].title,
-                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                            _bookmarks[index].title,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 4,
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
                           ),
-                          Text(bookmarkList[index].description,
-                              style: TextStyle(fontSize: 15, color: Colors.grey)),
+                          Text(_bookmarks[index].description,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.grey)),
                         ],
                       ),
                     ),
                   ),
                   Container(
                     padding: EdgeInsets.only(right: 12, top: 8, bottom: 8),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset( "assets/" +
-                            MyData.imageName[index],
-                          width: 170,
-                          height: 170,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      ),
-            ],
-          ),
-                ),
-              )),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(_bookmarks[index].urlToImage,
+                          width: 150, height: 150, fit: BoxFit.cover),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
         );
       },
     );
-
-  }
-  makeBookmarkList()
-  {
-    for (int i = 0; i < MyData.count; i++) {
-      bookmarkList[i] = MyData.getBookmark(i);
-    }
   }
 
-  /*_launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }*/
 }
 
 class DetailScreen extends StatelessWidget {
@@ -111,7 +140,8 @@ class DetailScreen extends StatelessWidget {
   final Bookmark bookmark;
 
   // In the constructor, require a Todo.
-  DetailScreen({Key key, @required this.bookmark}) : super(key: key);
+  DetailScreen({Key key, @required this.bookmark})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -122,9 +152,17 @@ class DetailScreen extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
-            Text(bookmark.title, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-            SizedBox(height: 20,),
-            Text(bookmark.description, style: TextStyle(fontSize: 20),),
+            Text(
+              bookmark.title,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              bookmark.description,
+              style: TextStyle(fontSize: 20),
+            ),
           ],
         ),
       ),
